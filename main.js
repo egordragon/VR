0237884862e3dabbd81c561d09568c40cbb9519b
egordragon <84422823+egordragon@users.noreply.github.com>
2023-05-18 14:36:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -94,40 +94,39 @@ function draw() {
   //let matAccum1 = m4.multiply(translateLeftEye, matAccum3)
   let matAccum3 = m4.multiply(orientationRotateMatrix, matAccum0)
   let matAccum2 = m4.multiply(translateToPointZero, matAccum3)
-  console.log(matAccum3)
 
-  // document.getElementById('matrix0').innerHTML =
-  //   'Matrix 0 elem ' + orientationRotateMatrix[0]
-  // document.getElementById('matrix1').innerHTML =
-  //   'Matrix 1 elem ' + orientationRotateMatrix[1]
-  // document.getElementById('matrix2').innerHTML =
-  //   'Matrix 2 elem ' + orientationRotateMatrix[2]
-  // document.getElementById('matrix3').innerHTML =
-  //   'Matrix 3 elem ' + orientationRotateMatrix[3]
-  // document.getElementById('matrix4').innerHTML =
-  //   'Matrix 4 elem ' + orientationRotateMatrix[4]
-  // document.getElementById('matrix5').innerHTML =
-  //   'Matrix 5 elem ' + orientationRotateMatrix[5]
-  // document.getElementById('matrix6').innerHTML =
-  //   'Matrix 6 elem ' + orientationRotateMatrix[6]
-  // document.getElementById('matrix7').innerHTML =
-  //   'Matrix 7 elem ' + orientationRotateMatrix[7]
-  // document.getElementById('matrix8').innerHTML =
-  //   'Matrix 8 elem ' + orientationRotateMatrix[8]
-  // document.getElementById('matrix9').innerHTML =
-  //   'Matrix 9 elem ' + orientationRotateMatrix[9]
-  // document.getElementById('matrix10').innerHTML =
-  //   'Matrix 10 elem ' + orientationRotateMatrix[10]
-  // document.getElementById('matrix11').innerHTML =
-  //   'Matrix 11 elem ' + orientationRotateMatrix[11]
-  // document.getElementById('matrix12').innerHTML =
-  //   'Matrix 12 elem ' + orientationRotateMatrix[12]
-  // document.getElementById('matrix13').innerHTML =
-  //   'Matrix 13 elem ' + orientationRotateMatrix[13]
-  // document.getElementById('matrix14').innerHTML =
-  //   'Matrix 14 elem ' + orientationRotateMatrix[14]
-  // document.getElementById('matrix15').innerHTML =
-  //   'Matrix 151 elem ' + orientationRotateMatrix[15]
+  document.getElementById('matrix0').innerHTML =
+    'Matrix 0 elem ' + orientationRotateMatrix[0]
+  document.getElementById('matrix1').innerHTML =
+    'Matrix 1 elem ' + orientationRotateMatrix[1]
+  document.getElementById('matrix2').innerHTML =
+    'Matrix 2 elem ' + orientationRotateMatrix[2]
+  document.getElementById('matrix3').innerHTML =
+    'Matrix 3 elem ' + orientationRotateMatrix[3]
+  document.getElementById('matrix4').innerHTML =
+    'Matrix 4 elem ' + orientationRotateMatrix[4]
+  document.getElementById('matrix5').innerHTML =
+    'Matrix 5 elem ' + orientationRotateMatrix[5]
+  document.getElementById('matrix6').innerHTML =
+    'Matrix 6 elem ' + orientationRotateMatrix[6]
+  document.getElementById('matrix7').innerHTML =
+    'Matrix 7 elem ' + orientationRotateMatrix[7]
+  document.getElementById('matrix8').innerHTML =
+    'Matrix 8 elem ' + orientationRotateMatrix[8]
+  document.getElementById('matrix9').innerHTML =
+    'Matrix 9 elem ' + orientationRotateMatrix[9]
+  document.getElementById('matrix10').innerHTML =
+    'Matrix 10 elem ' + orientationRotateMatrix[10]
+  document.getElementById('matrix11').innerHTML =
+    'Matrix 11 elem ' + orientationRotateMatrix[11]
+  document.getElementById('matrix12').innerHTML =
+    'Matrix 12 elem ' + orientationRotateMatrix[12]
+  document.getElementById('matrix13').innerHTML =
+    'Matrix 13 elem ' + orientationRotateMatrix[13]
+  document.getElementById('matrix14').innerHTML =
+    'Matrix 14 elem ' + orientationRotateMatrix[14]
+  document.getElementById('matrix15').innerHTML =
+    'Matrix 151 elem ' + orientationRotateMatrix[15]
 
   // First pass for left eye, drawing red component only)
 
@@ -328,9 +327,9 @@ function ReadGyroscope() {
       'Angular velocity along the Z-axis ' + sensor.z
     let current = e.timeStamp
     let dt = (current - timestamp) * NS2S
-    let x = sensor.x
-    let y = sensor.y
-    let z = sensor.z
+    let x = sensor.x * 80
+    let y = sensor.y * 80
+    let z = sensor.z * 80
 
     let eps = 0.3
     let angSpeed = Math.sqrt(x * x + y * y + z * z)
@@ -347,7 +346,7 @@ function ReadGyroscope() {
     deltaRotVec[0] = sinTheta * x
     deltaRotVec[1] = sinTheta * y
     deltaRotVec[2] = sinTheta * z
-    deltaRotVec[3] = sinTheta * z
+    deltaRotVec[3] = cosTheta * z
 
     let deltaRotationMatrix = Array(16)
 
