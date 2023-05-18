73fcc83a9c8f528c73242e83cbfe602589577d94
egordragon <84422823+egordragon@users.noreply.github.com>
2023-05-18 13:57:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -328,9 +328,9 @@ function ReadGyroscope() {
       'Angular velocity along the Z-axis ' + sensor.z
     let current = e.timeStamp
     let dt = (current - timestamp) * NS2S
-    let x = sensor.x
-    let y = sensor.y
-    let z = sensor.z
+    let x = sensor.x * 30
+    let y = sensor.y * 30
+    let z = sensor.z * 30
 
     let eps = 0.3
     let angSpeed = Math.sqrt(x * x + y * y + z * z)
@@ -344,10 +344,10 @@ function ReadGyroscope() {
     let cosTheta = Math.cos(thetaOverTwo)
 
     let deltaRotVec = Array(4)
-    deltaRotVec[0] = sinTheta * x * 10
-    deltaRotVec[1] = sinTheta * y * 10
-    deltaRotVec[2] = sinTheta * z * 10
-    deltaRotVec[3] = 1
+    deltaRotVec[0] = sinTheta * x
+    deltaRotVec[1] = sinTheta * y
+    deltaRotVec[2] = sinTheta * z
+    deltaRotVec[3] = cosTheta
 
     let deltaRotationMatrix = Array(16)
 
