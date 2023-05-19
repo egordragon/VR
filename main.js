7041a02c2356b14297b8f2388bdeb3481360fdd4
egordragon <84422823+egordragon@users.noreply.github.com>
2023-05-19 11:31:17 +0300

    Small changes to gyroscope func

--- main.js
+++ main.js
@@ -277,10 +277,10 @@ function ReadGyroscope() {
     let cosTheta = Math.cos(thetaOverTwo)
 
     let deltaRotVec = Array(4)
-    deltaRotVec[0] = sinTheta * x
-    deltaRotVec[1] = sinTheta * y
-    deltaRotVec[2] = sinTheta * z
-    deltaRotVec[3] = cosTheta * 100
+    deltaRotVec[0] = sinTheta * x * 100
+    deltaRotVec[1] = sinTheta * y * 100
+    deltaRotVec[2] = sinTheta * z * 100
+    deltaRotVec[3] = cosTheta
 
     let deltaRotationMatrix = Array(16)
 
